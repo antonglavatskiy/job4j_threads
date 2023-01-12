3f40b22dd4574838a2a4cab657576d545ca14540
antonglavatskiy <glavazky@yandex.ru>
2023-01-12 13:25:51 +0500

    4. Режим ожидания. [#231217]

---
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -0,0 +1,20 @@
+package ru.job4j.concurrent;
+
+public class Wget {
+    public static void main(String[] args) {
+        Thread load = new Thread(
+                () -> {
+                    try {
+                        for (int i = 0; i <= 100; i++) {
+                            System.out.print("\rLoading : " + i  + "%");
+                            Thread.sleep(1000);
+                        }
+                        System.out.println("\nLoad completed");
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+        );
+        load.start();
+    }
+}
